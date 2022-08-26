1df060ea8f31f79ea6db92a6737553dfafb57b8f
leeheewon-01 <81808758+leeheewon-01@users.noreply.github.com>
2022-08-26 23:10:01 +0900

    Create man_detect.py

---
+++ man_detect.py
@@ -0,0 +1,40 @@
+# import necessary packages
+import cvlib as cv
+from cvlib.object_detection import draw_bbox
+import cv2
+
+# open webcam (웹캠 열기)
+webcam = cv2.VideoCapture(0)
+
+if not webcam.isOpened():
+    print("Could not open webcam")
+    exit()
+    
+
+# loop through frames
+while webcam.isOpened():
+
+    # read frame from webcam 
+    status, frame = webcam.read()
+
+    if not status:
+        break
+
+    # apply object detection (물체 검출)
+    bbox, label, conf = cv.detect_common_objects(frame)
+
+    print(bbox, label, conf)
+
+    # draw bounding box over detected objects (검출된 물체 가장자리에 바운딩 박스 그리기)
+    out = draw_bbox(frame, bbox, label, conf, write_conf=True)
+
+    # display output
+    cv2.imshow("Real-time object detection", out)
+
+    # press "Q" to stop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+# release resources
+webcam.release()
+cv2.destroyAllWindows()   
